a21dbf0bc9b656776f066e820394a4af33a85c2f
kozhemyakin <pctdoc@gmail.com>
2022-12-22 14:45:11 +0300

    feat: add solution for the task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const qty = expr.length / 10;
+    let symbol;
+    let arr = [];
+
+    for (let i = 1, j = 0; i <= qty; i++, j+=10) {
+        symbol = expr.substring( 0 + j, 10 + j); // 10, 19
+
+        const morseLetter = symbol.replaceAll('00', '').replaceAll('10', '.').replaceAll('11', '-')
+
+        for (const [key, value] of Object.entries(MORSE_TABLE)) {
+            if (key === morseLetter) {
+                
+                arr.push(value)
+            }
+        }
+
+        if (morseLetter === '**********') {
+            arr.push(' ')
+        }        
+    }
+
+    return arr.join('')
 }
 
 module.exports = {
